fef88f8bc259a8727c6eb4246a0667f304861ff6
Marcelo Felix <mocfelix@gmail.com>
2026-01-09 05:12:38 +0000

    Initial commit of Brainsty Health Navigator

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  "realtimeDatabaseInstanceUri": "",
+  "realtimeDatabaseUrl": "",
+  "storageBucket": "studio-2671301810-e0a76.firebasestorage.app",
+  "locationId": "",
+  "apiKey": "AIzaSyDC886khiOxX49cam0F9QfIUJzzmsvBSgU",
+  "authDomain": "studio-2671301810-e0a76.firebaseapp.com",
+  "messagingSenderId": "381571846140",
+  "measurementId": ""
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
